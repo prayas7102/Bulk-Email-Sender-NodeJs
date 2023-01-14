9fb267eafd5778a7403d24f6ebe02e1455b9853d
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2023-01-14 18:29:15 +0530

    getmail router setup

--- Backend/routes/EmailRouter.js
+++ Backend/routes/EmailRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { SendEmail } = require("../controller/EmailController")
+const { SendEmail, GetEmail } = require("../controller/EmailController")
 
 router.route('/send-email').post(SendEmail);
+router.route('/get-emails').get(GetEmail);
 
 module.exports = router;
