bc34136f60b774cfa34577b103b021804e9620cb
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2023-01-16 00:45:08 +0530

    minor changes for deployement

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const cors = require('cors');
 app.use(cors());
@@ -12,12 +13,11 @@ const { databaseConnect } = require('./config/database');
 databaseConnect();
 
 app.use(express.json());
+const port = process.env.PORT || 5000;
 
 const EmailRouter = require('./routes/EmailRouter');
 app.use('/api/user/', EmailRouter);
 
-// app.use(cookieParser());
-
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on ${process.env.PORT}`)
+app.listen(port, () => {
+    console.log(`Server is running on ${port}`)
 });
