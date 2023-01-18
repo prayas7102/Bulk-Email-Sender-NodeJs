b9a8cbda325f48004695c0c5ba1dcc5f301fe892
Prayas Kumar <prayas.prithvirajpratap7@gmail.com>
2023-01-18 11:10:32 +0530

    console

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 exports.databaseConnect = async() => {
-    console.log(process.env.MONGO_URI)
+    console.log(typeof(process.env.MONGO_URI))
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
